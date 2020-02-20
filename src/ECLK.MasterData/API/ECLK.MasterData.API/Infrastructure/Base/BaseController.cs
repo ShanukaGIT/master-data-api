32320e23ec78fc186f0224752828cf1c8b548a75
Yasassri Purnajith <purnajith@outlook.com>
2020-02-20 20:51:08 +0530

    Base controller setup

---
+++ src/ECLK.MasterData/API/ECLK.MasterData.API/Infrastructure/Base/BaseController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECLK.MasterData.API.Infrastructure.Repositeries.MasterDB;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ECLK.MasterData.API.Infrastructure.Base
+{
+	public class BaseController<T> : ControllerBase
+	{
+		#region  Members
+
+		protected readonly IDBRepository							_repository;
+		protected readonly ILogger<T>								_logger;
+
+		#endregion
+
+		#region Constructor
+		
+		/// <summary>
+		/// Loads controller with the db repository 
+		/// </summary>
+		/// <param name="repository">DB Repository instance</param>
+		public BaseController (IDBRepository repository)
+		{
+			this._repository = repository;
+		}
+		
+		/// <summary>
+		/// Loads controller with the db repository and the logger
+		/// </summary>
+		/// <param name="repository">DB Repository instance</param>
+		/// <param name="logger">Logger instance</param>
+		public BaseController (IDBRepository repository, ILogger<T> logger)
+		{
+			this._repository = repository;
+			this._logger = logger;
+		}
+
+		#endregion
+	}
+}
