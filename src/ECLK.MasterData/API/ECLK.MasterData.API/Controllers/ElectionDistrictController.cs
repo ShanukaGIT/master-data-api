e5935addf96793865a730b3b3f87119a7710e0e7
Yasassri Purnajith <purnajith@outlook.com>
2020-02-24 11:53:12 +0530

    Election DIstrict Controller, Polling Division Controller creation

---
+++ src/ECLK.MasterData/API/ECLK.MasterData.API/Controllers/ElectionDistrictController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECLK.MasterData.API.Infrastructure.Base;
+using ECLK.MasterData.API.Infrastructure.Repositeries.MasterDB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECLK.MasterData.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ElectionDistrictController : BaseController<ElectionDistrictController>
+    {
+		#region Constructor
+
+		public ElectionDistrictController (IDBRepository repository) : base(repository)
+		{
+
+		}
+
+		#endregion
+		
+		
+		// GET: api/ElectionDistrict
+		/// <summary>
+		/// List election districts
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+        public object Get()
+        {
+			return this._repository.Get("spElectionDistrictAll");
+        }
+
+        // GET: api/ElectionDistrict/5
+		/// <summary>
+		/// Get election district for the given ID
+		/// </summary>
+		/// <param name="adminDistrictID"></param>
+		/// <returns></returns>
+        [HttpGet("{adminDistrictID}", Name = "GetElectionDistrctByAdminDistrictID")]
+		public object Get(int adminDistrictID)
+        {
+			List<KeyValuePair<string, object>> parameters =  new List<KeyValuePair<string, object>>();
+
+			parameters.Add(new KeyValuePair<string, object>("@adisid", adminDistrictID));
+
+           return this._repository.Get("spEDisByADisID", parameters);
+        }
+    }
+}
---
+++ src/ECLK.MasterData/API/ECLK.MasterData.API/Controllers/PollingDivisionController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using ECLK.MasterData.API.Infrastructure.Base;
+using ECLK.MasterData.API.Infrastructure.Repositeries.MasterDB;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECLK.MasterData.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PollingDivisionController : BaseController<PollingDivisionController>
+    {
+		#region Constructor
+
+		public PollingDivisionController (IDBRepository repository) : base(repository)
+		{
+
+		}
+
+		#endregion
+		
+		
+		// GET: api/PollingDivision
+		/// <summary>
+		/// List polling division
+		/// </summary>
+		/// <returns></returns>
+		[HttpGet]
+        public object Get()
+        {
+			return this._repository.Get("spPDivByADisID");
+        }
+
+        // GET: api/PollingDivision/5
+		/// <summary>
+		/// Get polling division for the given ID
+		/// </summary>
+		/// <param name="adminDistrictID"></param>
+		/// <returns></returns>
+        [HttpGet("{adminDistrictID}", Name = "GetPollingDivisionByAdminDistrictID")]
+		public object Get(int adminDistrictID)
+        {
+			List<KeyValuePair<string, object>> parameters =  new List<KeyValuePair<string, object>>();
+
+			parameters.Add(new KeyValuePair<string, object>("@edisid", adminDistrictID));
+
+           return this._repository.Get("spPollingDivisionByEDisVR", parameters);
+        }
+    }
+}
